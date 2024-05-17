076fec4b9319c1a620cac9456eb5234d335e72d1
kauecurti <36936274+kauecurti@users.noreply.github.com>
2024-05-17 00:10:08 -0300

    Update consumers.py

--- video_analysis/consumers.py
+++ video_analysis/consumers.py
@@ -1,5 +1,3 @@
-# video_analysis/consumers.py
-
 import json
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
@@ -10,7 +8,7 @@ class VideoConsumer(WebsocketConsumer):
         self.video_id = self.scope['url_route']['kwargs']['video_id']
         self.room_group_name = f'video_{self.video_id}'
 
-        # Join room group
+        
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
@@ -29,7 +27,7 @@ class VideoConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         progress = text_data_json['progress']
 
-        # Send message to room group
+      
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
@@ -41,7 +39,7 @@ class VideoConsumer(WebsocketConsumer):
     def video_progress(self, event):
         progress = event['progress']
 
-        # Send message to WebSocket
+       
         self.send(text_data=json.dumps({
             'progress': progress
         }))
